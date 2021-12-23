69dd04662502349084a8eacf8e224f711347aa56
samueltanner <samuel.m.s.tanner@gmail.com>
2021-12-23 10:16:31 -0800

    fixes centering of job history, breaks container for personal description

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import JobCard from "./components/JobDescription/JobCard";
 import Profile from "./components/Profile/Profile";
 import Timeline from "./components/Timeline/Timeline";
 
 function App() {
+  const [job, setCurrentJob] = useState(0);
+  const selectJob = (job) => {
+    setCurrentJob(job);
+  };
   return (
-    <div className="h-screen bg-grey   md:flex gap-4">
+    <div className="h-screen bg-grey md:flex gap-0">
       <div className="flex flex-col gap-2 p-2 md:w-1/2">
         <Profile />
-        <JobCard />
+        <JobCard currentJob={job} />
       </div>
       <div className="mt-3 md:mt-0 flex-1 flex overflow-hidden pt-2">
-        <div className="md:flex-1 overflow-y-scroll px-1">
-          <Timeline />
+        <div className="md:flex-1 mx-auto lg:overflow-y-scroll">
+          <Timeline selectJob={selectJob} />
         </div>
       </div>
     </div>

59c4627b09c0feefe077a34de43289ff1f4a91b8
samueltanner <samuel.m.s.tanner@gmail.com>
2021-12-28 15:13:22 -0800

    adds .env file for email keys

--- src/ContactBar/ContactCard.js
+++ src/ContactBar/ContactCard.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { send } from "emailjs-com";
 import Card from "../components/UI/Card";
 import RectangleButton from "../components/UI/RectangleButton";
 
 const ContactCard = (props) => {
+  const form = useRef();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
@@ -20,6 +22,27 @@ const ContactCard = (props) => {
 
   const handleSubmitForm = (event) => {
     event.preventDefault();
+
+    let form = {
+      from_name: name,
+      reply_to: email,
+      to_name: "Sam Tanner",
+      message: message,
+    };
+
+    send(
+      process.env.REACT_APP_EMAIL_SERVICE,
+      process.env.REACT_APP_EMAIL_TEMPLATE,
+      form,
+      process.env.REACT_APP_EMAIL_USER_ID
+    )
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
     setName("");
     setEmail("");
     setMessage("");
@@ -33,8 +56,9 @@ const ContactCard = (props) => {
 
   return (
     <Card className="bg-slate-200 drop-shadow-lg h-1/2 relative">
-      <form className="flex flex-col" onSubmit={handleSubmitForm}>
+      <form className="flex flex-col" onSubmit={handleSubmitForm} ref={form} id="contact_form">
         <label>Name:</label>
+        {process.env.EMAIL_SERVICE}
         <input type="text" onChange={handleChangeName} value={name} className="input-primary" required />
 
         <label>Email:</label>

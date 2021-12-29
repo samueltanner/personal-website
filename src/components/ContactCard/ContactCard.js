7af9753838bdccdb2505483b5ca8e1e075166400
samueltanner <samuel.m.s.tanner@gmail.com>
2021-12-29 11:42:13 -0800

    more minor fixes

--- src/components/ContactCard/ContactCard.js
+++ src/components/ContactCard/ContactCard.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import { send } from "emailjs-com";
 import Card from "../UI/Card";
 import RectangleButton from "../UI/RectangleButton";

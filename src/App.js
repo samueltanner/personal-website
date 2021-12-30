e50e71f9abeecc40857b44883d275ea7879ffbd5
samueltanner <samuel.m.s.tanner@gmail.com>
2021-12-29 16:44:10 -0800

    gets toolbar to show up relative to screen size

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import JobCard from "./components/JobDescription/JobCard";
 import Profile from "./components/Profile/Profile";
 import Timeline from "./components/Timeline/Timeline";
 import ContactCard from "./components/ContactCard/ContactCard";
+import FooterMenuCard from "./components/FooterMenu/FooterMenuCard";
 
 function App() {
   const [job, setCurrentJob] = useState(0);
@@ -20,12 +21,14 @@ function App() {
       <div className="flex flex-col p-2 md:w-1/2" id="profile">
         <Profile toggleContact={toggleContact} showContactCard={showContactCard} />
         {showContactCard ? <ContactCard toggleContact={toggleContactCard} /> : <JobCard currentJob={job} />}
+        <FooterMenuCard className="visible md:hidden mx-1 mt-1" />
       </div>
-      <div className="mt-3 md:mt-0 flex-1 flex overflow-hidden pt-2">
-        <div className="md:flex-1 mx-auto overflow-y-scroll">
+      <div className=" mt-3 md:mt-0 flex-1 flex overflow-hidden pt-2">
+        <div className="relative md:flex-1 mx-auto overflow-y-scroll ">
           <Timeline selectJob={selectJob} toggleContact={toggleContact} showContactCard={showContactCard} />
         </div>
       </div>
+      <FooterMenuCard className="invisible h-0 md:visible md:h-auto" />
     </div>
   );
 }

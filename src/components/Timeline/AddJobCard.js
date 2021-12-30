c6dc5216fc74acc79c1933110463c89a36f82869
samueltanner <samuel.m.s.tanner@gmail.com>
2021-12-30 09:20:34 -0800

    fixes z-index for add job card

--- src/components/Timeline/AddJobCard.js
+++ src/components/Timeline/AddJobCard.js
@@ -5,7 +5,7 @@ const AddJobCard = (props) => {
       onClick={!props.showContactCard ? props.toggleContact : null}
     >
       <div className="order-1 w-5/12"></div>
-      <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+      <div className="z-10 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
         <h1 className="mx-auto font-semibold text-lg text-white">+</h1>
       </div>
       <div className="order-1 bg-teal-400 rounded-lg shadow-xl w-5/12 px-6 py-4 hover:bg-teal-200">

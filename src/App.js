a2560bb941354790a851a156fa2e1bd8fc346038
samueltanner <samuel.m.s.tanner@gmail.com>
2021-12-29 20:41:25 -0800

    gets sticky header to work on menu bar

--- src/App.js
+++ src/App.js
@@ -21,10 +21,10 @@ function App() {
       <div className="flex flex-col p-2 md:w-1/2" id="profile">
         <Profile toggleContact={toggleContact} showContactCard={showContactCard} />
         {showContactCard ? <ContactCard toggleContact={toggleContactCard} /> : <JobCard currentJob={job} />}
-        <FooterMenuCard className="visible md:hidden mx-1 mt-1" />
       </div>
-      <div className="mt-3 md:mt-0 flex-1 flex overflow-hidden pt-0">
-        <div className="relative md:flex-1 mx-auto overflow-y-scroll p-0 ">
+      <div className="mt-3 md:mt-0 flex-1 flex md:overflow-hidden pt-0">
+        <div className="relative md:flex-1 mx-auto md:overflow-y-scroll p-0 ">
+          <FooterMenuCard className="z-50 sticky top-3 visible md:hidden mx-3 mt-0" />
           <Timeline selectJob={selectJob} toggleContact={toggleContact} showContactCard={showContactCard} />
         </div>
       </div>

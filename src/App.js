b9c5aca730e316f61110e26fb970fcd336591f7a
samueltanner <samuel.m.s.tanner@gmail.com>
2021-12-30 14:34:23 -0800

    makes footer menu reactive

--- src/App.js
+++ src/App.js
@@ -10,11 +10,17 @@ import Skills from "./components/Skills/Skills";
 function App() {
   const [job, setCurrentJob] = useState(0);
   const [showContactCard, toggleContactCard] = useState(false);
+  const [currentWindow, setCurrentWindow] = useState("timeline");
   const selectJob = (job) => {
     setCurrentJob(job);
   };
+
   const toggleContact = () => {
     toggleContactCard(!showContactCard);
+  };
+
+  const setWindow = (selection) => {
+    setCurrentWindow(selection);
   };
 
   return (
@@ -25,13 +31,15 @@ function App() {
       </div>
       <div className="mt-3 md:mt-0 flex-1 flex md:overflow-hidden pt-0">
         <div className="relative md:flex-1 mx-auto md:overflow-y-scroll p-0 ">
-          <FooterMenuCard className="z-50 sticky top-3 visible md:hidden mx-3 mt-0" />
-          {/* <Timeline selectJob={selectJob} toggleContact={toggleContact} showContactCard={showContactCard} /> */}
-          <Skills />
+          <FooterMenuCard className="z-50 sticky top-3 visible md:hidden mx-3 mt-0" setWindow={setWindow} />
+          {currentWindow === "skills" && <Skills />}
+          {currentWindow === "timeline" && (
+            <Timeline selectJob={selectJob} toggleContact={toggleContact} showContactCard={showContactCard} />
+          )}
         </div>
       </div>
       <div className="hidden md:block">
-        <FooterMenuCard />
+        <FooterMenuCard setWindow={setWindow} />
       </div>
     </div>
   );
--- src/components/FooterMenu/FooterMenuCard.js
+++ src/components/FooterMenu/FooterMenuCard.js
@@ -1,13 +1,23 @@
 const FooterMenuCard = (props) => {
+  const handleMenuSelection = (event) => {
+    props.setWindow(event.target.value);
+  };
+
   return (
     <div
       className={`md:fixed md:bottom-3 bg-slate-200 rounded-xl drop-shadow-lg z-20 left-2 right-2 md:left-1/2 md:right-3 ${props.className}`}
     >
       {/* <div className="fixed bottom-3 bg-slate-800 w-1/3 flex flex-wrap inset-x-0 mx-auto rounded-md drop-shadow-lg "> */}
-      <div className="flex flex-row gap-6 justify-center p-3">
-        <button>Work History</button>
-        <button>Skills</button>
-        <button>Portfolio</button>
+      <div className="flex flex-row gap-6 justify-around p-3">
+        <button value="timeline" onClick={handleMenuSelection}>
+          Work History
+        </button>
+        <button value="skills" onClick={handleMenuSelection}>
+          Skills
+        </button>
+        {/* <button value="portfolio" onClick={handleMenuSelection}>
+          Portfolio
+        </button> */}
       </div>
     </div>
   );

a0cdfc23a05c9296a86104f804f4cc2908a95ddc
samueltanner <samuel.m.s.tanner@gmail.com>
2021-12-30 17:24:42 -0800

    fixes footer menu button to toggle and to highlight current selection

--- src/components/FooterMenu/FooterMenuCard.js
+++ src/components/FooterMenu/FooterMenuCard.js
@@ -1,18 +1,31 @@
+import { useState } from "react/cjs/react.development";
+
 const FooterMenuCard = (props) => {
+  const [currentWindow, setCurrentWindow] = useState("timeline");
+
   const handleMenuSelection = (event) => {
+    setCurrentWindow(event.target.value);
     props.setWindow(event.target.value);
   };
 
   return (
     <div
-      className={`bg-slate-200 rounded-xl drop-shadow-lg z-20 left-2 right-2 mx-3 md:left-1/2 md:right-3 md:fixed md:bottom-3 ${props.className}`}
+      className={`bg-slate-200 rounded-xl drop-shadow-lg z-20 left-2 right-2 mx-3 md:left-1/2  md:right-3 md:fixed md:bottom-3 ${props.className} `}
     >
-      {/* <div className="fixed bottom-3 bg-slate-800 w-1/3 flex flex-wrap inset-x-0 mx-auto rounded-md drop-shadow-lg "> */}
-      <div className="flex flex-row gap-6 justify-around p-3">
-        <button value="timeline" onClick={handleMenuSelection}>
+      <div className="flex flex-row gap-6 justify-around p-3 ">
+        <button
+          value="timeline"
+          className={`${currentWindow === "timeline" ? "font-bold" : null} w-1/2`}
+          onClick={handleMenuSelection}
+        >
+          {/* <p className={currentWindow === "timeline" ? "font-bold" : null}>Work History</p> */}
           Work History
         </button>
-        <button value="skills" onClick={handleMenuSelection}>
+        <button
+          value="skills"
+          className={`${currentWindow === "skills" ? "font-bold" : null} w-1/2`}
+          onClick={handleMenuSelection}
+        >
           Skills
         </button>
         {/* <button value="portfolio" onClick={handleMenuSelection}>

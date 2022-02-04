22041524256e517a95ef3d82fe1f6c5ed1016e74
samueltanner <samuel.m.s.tanner@gmail.com>
2022-02-04 15:12:52 -0800

    fixes image width

--- src/components/Dogs/DogImage.js
+++ src/components/Dogs/DogImage.js
@@ -4,6 +4,9 @@ import Button from "../UI/RectangleButton";
 
 const DogImage = ({ url }) => {
   const [image, setImage] = useState("");
+  const click = () => {
+    return;
+  };
   useEffect(() => {
     fetch("https://dog.ceo/api/breeds/image/random")
       .then((results) => results.json())
@@ -14,7 +17,7 @@ const DogImage = ({ url }) => {
 
   return (
     <div className="flex flex-col justify-center">
-      <img src={image} alt="Dog" className=""></img>
+      <img src={image} alt="Dog" className="w-100"></img>
       <Button buttonType="bg-[#95A3B8] w-40" text="Woof!" className="max-w-100" />
     </div>
   );

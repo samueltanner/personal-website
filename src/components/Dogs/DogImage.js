cc50795d4db394438c078dc5e3549b34e2994340
samueltanner <samuel.m.s.tanner@gmail.com>
2022-02-04 15:17:54 -0800

    get dog image on click

--- src/components/Dogs/DogImage.js
+++ src/components/Dogs/DogImage.js
@@ -4,21 +4,21 @@ import Button from "../UI/RectangleButton";
 
 const DogImage = ({ url }) => {
   const [image, setImage] = useState("");
-  const click = () => {
-    return;
-  };
-  useEffect(() => {
+
+  const getImage = () => {
     fetch("https://dog.ceo/api/breeds/image/random")
       .then((results) => results.json())
       .then((data) => {
         setImage(data.message);
       });
-  }, []);
+  };
+
+  useEffect(getImage, []);
 
   return (
     <div className="flex flex-col justify-center">
-      <img src={image} alt="Dog" className="w-100"></img>
-      <Button buttonType="bg-[#95A3B8] w-40" text="Woof!" className="max-w-100" />
+      <img src={image} alt="Dog" className="h-100"></img>
+      <Button buttonType="bg-[#95A3B8] w-40" text="Woof!" onClick={getImage} />
     </div>
   );
 };
